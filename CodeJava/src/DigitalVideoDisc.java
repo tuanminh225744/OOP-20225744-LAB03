64cbca5afff3a1925c844f12dcb0b95b35b27a09
Phạm Tuấn Minh <tuanminh100217@gmail.com>
2024-11-26 20:19:55 +0700

    bai4

--- CodeJava/src/DigitalVideoDisc.java
+++ CodeJava/src/DigitalVideoDisc.java
@@ -4,6 +4,10 @@ public class DigitalVideoDisc {
     private String director;
     private int length;
     private float cost;
+    
+    public static int nbDigitalVideoDiscs = 0; 
+    private int id;
+    
 	public String getTitle() {
 		return title;
 	}
@@ -19,18 +23,27 @@ public class DigitalVideoDisc {
 	public float getCost() {
 		return cost;
 	}
+	public float getId() {
+		return id;
+	}
 	public void setTitle(String title) {
 	    this.title = title;
 	}
 	public DigitalVideoDisc(String title) {
 		super();
 		this.title = title;
+		
+		nbDigitalVideoDiscs++;
+		this.id = nbDigitalVideoDiscs;
 	}
 	public DigitalVideoDisc(String title, String category, float cost) {
 		super();
 		this.title = title;
 		this.category = category;
 		this.cost = cost;
+		
+		nbDigitalVideoDiscs++;
+		this.id = nbDigitalVideoDiscs;
 	}
 	public DigitalVideoDisc(String title, String category, String director, float cost) {
 		super();
@@ -38,6 +51,9 @@ public class DigitalVideoDisc {
 		this.category = category;
 		this.director = director;
 		this.cost = cost;
+
+		nbDigitalVideoDiscs++;
+		this.id = nbDigitalVideoDiscs;
 	}
 	public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
 		super();
@@ -46,6 +62,9 @@ public class DigitalVideoDisc {
 		this.director = director;
 		this.length = length;
 		this.cost = cost;
+
+		nbDigitalVideoDiscs++;
+		this.id = nbDigitalVideoDiscs;
 	}
 	
 }
